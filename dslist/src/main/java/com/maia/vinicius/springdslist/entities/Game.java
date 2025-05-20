c9baf539aa1c5f35bad5c09d9aa2dcddfc8de778
Vinícius Holanda <75538677+holandavinicius@users.noreply.github.com>
2025-05-20 19:20:29 +0100

    Game seed

--- dslist/src/main/java/com/maia/vinicius/springdslist/entities/Game.java
+++ dslist/src/main/java/com/maia/vinicius/springdslist/entities/Game.java
@@ -12,24 +12,30 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private Double score;
+    private String platforms;
 
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
     private String imgUrl;
+
+    @Column(columnDefinition ="TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition ="TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Double score, String platforms, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
+        this.score = score;
+        this.platforms = platforms;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -59,6 +65,14 @@ public class Game {
         this.year = year;
     }
 
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
     public String getGenre() {
         return genre;
     }
@@ -67,20 +81,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
-    }
-
-    public void setPlatform(String platform) {
-        this.platform = platform;
-    }
-
     public String getImgUrl() {
         return imgUrl;
     }
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getShortDescription() {
