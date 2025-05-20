fc037593a2e61966f9cc10926e8cd113f632d419
Vinícius Holanda <75538677+holandavinicius@users.noreply.github.com>
2025-05-20 07:50:04 +0100

    Project Setup

---
+++ dslist/src/test/java/com/maia/vinicius/springdslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maia.vinicius.springdslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
